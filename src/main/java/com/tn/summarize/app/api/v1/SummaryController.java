a157b11318a36beae0233c54c1260f86c197ee6d
nick <nick.holt@btinternet.com>
2024-11-07 13:26:18 +0000

    Basic file uploading working.

--- src/main/java/com/tn/summarize/app/api/v1/SummaryController.java
+++ src/main/java/com/tn/summarize/app/api/v1/SummaryController.java
@@ -1,9 +1,26 @@
 package com.tn.summarize.app.api.v1;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController("/v1/summary")
+import com.tn.summarize.app.domain.Summary;
+
+@Slf4j
+@RestController
+@RequestMapping("/v1/summary")
 public class SummaryController
 {
-
+  @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  Summary create(@AuthenticationPrincipal Jwt jwt, @RequestPart("file") MultipartFile file, @RequestParam(name = "folderId", required = false) Long folderId)
+  {
+    return new Summary(1L, file.getOriginalFilename());
+  }
 }
--- src/main/java/com/tn/summarize/app/domain/Summary.java
+++ src/main/java/com/tn/summarize/app/domain/Summary.java
@@ -1,26 +1,3 @@
 package com.tn.summarize.app.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-
-//@Entity
-//@Table("summary")
-//@Cacheable(false)
-//@NoArgsConstructor
-//@Accessors(fluent = true)
-//@Getter
-//@EqualsAndHashCode
-//@ToString
-public class Summary
-{
-  @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "summaryId")
-  @SequenceGenerator(name = "summaryId", sequenceName = "summary_id_seq", allocationSize = 1)
-  @Column(name = "summary_id")
-  private Long id;
-
-
-}
+public record Summary(Long id, String name) {}
